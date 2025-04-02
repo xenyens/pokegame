ed8e0cd996ce96d144b0fb16a157d9e3dac7e1dd
Xenia Padilla <xeny@MacBook-Air-de-Xenia.local>
2025-04-01 19:12:34 -0700

    game poke2

--- src/modules/pokemon/composables/usePokemonGame.ts
+++ src/modules/pokemon/composables/usePokemonGame.ts
@@ -1,9 +1,15 @@
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { GameStatus, type PokemonListResponse, type Pokemon } from '../interfaces';
 import { pokemonApi } from '../api/pokemonAPI';
 
 export const usePokemonGame = () => {
   const gameStatus = ref<GameStatus>(GameStatus.Playing);
+  const pokemones = ref<Pokemon[]>([]);
+  const theChosenOnes = ref<Pokemon[]>([]);
+  const isLoading = computed(() => pokemones.value.length === 0);
+  const randomPokemon = computed(
+    () => theChosenOnes.value[Math.floor(Math.random() * theChosenOnes.value.length)],
+  ); //Un pokemon de theChosenOnes
 
   const getPokemons = async (): Promise<Pokemon[]> => {
     const response = await pokemonApi.get<PokemonListResponse>('/?limit=200');
@@ -18,15 +24,28 @@ export const usePokemonGame = () => {
       };
     });
 
-    return pokemonArray;
+    return pokemonArray.sort(() => Math.random() - 0.5);
+  };
+
+  const getPokemonOptions = (howMany: number = 4) => {
+    gameStatus.value = GameStatus.Playing;
+    theChosenOnes.value = pokemones.value.slice(0, howMany);
+    pokemones.value = pokemones.value.slice(howMany);
   };
 
   onMounted(async () => {
-    const pokemones = await getPokemons();
-    console.log(pokemones);
+    pokemones.value = await getPokemons();
+    getPokemonOptions();
+    console.log(theChosenOnes.value);
   });
 
   return {
     gameStatus,
+    isLoading,
+    theChosenOnes,
+    randomPokemon,
+
+    //methods
+    getPokemonOptions,
   };
 };
--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -3,17 +3,19 @@ import PokemonImage from '../components/PokemonImage.vue';
 import PokemonOptions from '../components/PokemonOptions.vue';
 import { usePokemonGame } from '../composables/usePokemonGame';
 
-const { gameStatus } = usePokemonGame();
+const { gameStatus, isLoading, randomPokemon } = usePokemonGame();
 </script>
 
 <template>
-  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="text-3xl">Espere por favor</h1>
     <h3>Cargando pokemons</h3>
   </section>
 
   <section class="flex flex-col justify-center items-center w-screen h-screen">
     <h1>¿Quién soy?</h1>
+
+    <h3>{{ randomPokemon }}</h3>
 
     <!-- Pokemon picture -->
     <PokemonImage />
